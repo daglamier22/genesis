87e9390dac38e1bef25b28052df483d76d283e0f
Brian McCarthy <daglamier22@gmail.com>
2023-11-24 16:46:45 -0600

    add trace level logging

--- genesis/src/Core/Logger.h
+++ genesis/src/Core/Logger.h
@@ -2,11 +2,12 @@
 
 namespace Genesis {
     enum LoggingLevel : uint8_t {
-        GN_DEBUG = 3U,
-        GN_INFO = 4U,
-        GN_WARNING = 5U,
-        GN_ERROR = 6U,
-        GN_CRITICAL = 7U
+        GN_LOGLEVEL_TRACE = 2U,
+        GN_LOGLEVEL_DEBUG = 3U,
+        GN_LOGLEVEL_INFO = 4U,
+        GN_LOGLEVEL_WARNING = 5U,
+        GN_LOGLEVEL_ERROR = 6U,
+        GN_LOGLEVEL_CRITICAL = 7U
     };
 
     class Logger {
@@ -32,9 +33,11 @@ namespace Genesis {
 #define GN_CORE_WARNING(...) QUILL_LOG_WARNING(::Genesis::Logger::getCoreLogger(), __VA_ARGS__)
 #define GN_CORE_INFO(...) QUILL_LOG_INFO(::Genesis::Logger::getCoreLogger(), __VA_ARGS__)
 #define GN_CORE_DEBUG(...) QUILL_LOG_DEBUG(::Genesis::Logger::getCoreLogger(), __VA_ARGS__)
+#define GN_CORE_TRACE(...) QUILL_LOG_TRACE_L1(::Genesis::Logger::getCoreLogger(), __VA_ARGS__)
 
 #define GN_CLIENT_CRITICAL(...) QUILL_LOG_CRITICAL(::Genesis::Logger::getClientLogger(), __VA_ARGS__)
 #define GN_CLIENT_ERROR(...) QUILL_LOG_ERROR(::Genesis::Logger::getClientLogger(), __VA_ARGS__)
 #define GN_CLIENT_WARNING(...) QUILL_LOG_WARNING(::Genesis::Logger::getClientLogger(), __VA_ARGS__)
 #define GN_CLIENT_INFO(...) QUILL_LOG_INFO(::Genesis::Logger::getClientLogger(), __VA_ARGS__)
 #define GN_CLIENT_DEBUG(...) QUILL_LOG_DEBUG(::Genesis::Logger::getClientLogger(), __VA_ARGS__)
+#define GN_CLIENT_TRACE(...) QUILL_LOG_TRACE_L1(::Genesis::Logger::getClientLogger(), __VA_ARGS__)
