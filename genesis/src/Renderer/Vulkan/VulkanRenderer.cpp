9fe46cdf6d83041ae3ef5f2ba0db7f42653c0dbc
Brian McCarthy <daglamier22@gmail.com>
2023-12-16 13:38:05 -0600

    add stb_image as a dependency

--- genesis/src/Renderer/Vulkan/VulkanRenderer.cpp
+++ genesis/src/Renderer/Vulkan/VulkanRenderer.cpp
@@ -1,5 +1,12 @@
 #include "VulkanRenderer.h"
 
+#if defined(__GNUC__) && !defined(NDEBUG) && defined(__OPTIMIZE__)
+    #undef __OPTIMIZE__
+#endif
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 #include <chrono>
 
 #include "Core/Logger.h"
